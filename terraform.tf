7a3f255e9724fde8ceb2af08d89252d28b55d1d1
balajiblj036 <balajiblj036@gmail.com>
2019-12-07 22:07:15 +0530

    terra

---
+++ terraform.tf
@@ -0,0 +1,44 @@
+provider "aws"  {
+ region  = "us-east-2"
+ provider "aws"  {
+ region  = "us-east-2"access_key = "AKIAVUDRUYFLPKH3QSH3"
+ secret_key = "ZtzHcMtSiKpLUlyarKhT2M2pojBJenvSxCXouBDe"
+}
+resource "aws_instance" "example0" {
+  ami = "ami-0e0bae59dc35fe89a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03cd8b41362c3cff3"
+}
+
+output "instance_type" {
+   value = "${aws_instance.example0.instance_type}"
+}
+
+resource "aws_instance" "example1" {
+  ami = "ami-0e0bae59dc35fe89a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03cd8b41362c3cff3"
+}
+
+resource "aws_instance" "example2" {
+  ami = "ami-0e0bae59dc35fe89a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03cd8b41362c3cff3"
+}
+
+resource "aws_instance" "example3" {
+  ami = "ami-0e0bae59dc35fe89a"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-03cd8b41362c3cff3"
+}
+root@LAPTOP-OOFKB8CO:~/scripts/terraform-templates#
+
+
+
+
+
+
+
+
+
+
